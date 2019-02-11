4f10f6485e33b0b2081facf954a4f12508736d14
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-02-11 22:09:52 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,10 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
 
-
+  def initialize(title, schedule, desc)
+    @title = title
+    @schedule = schedule
+    @description = desc
+    @@all << self
 end
